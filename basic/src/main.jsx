90950046278d9b959667d67e250c5eab4dbb2f25
Amitabh Singh <127296523+optimus-pride@users.noreply.github.com>
2025-02-06 23:29:47 +0530

    Delete basic directory

--- basic/src/main.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable-next-line no-unused-vars */
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <App/>
-)
